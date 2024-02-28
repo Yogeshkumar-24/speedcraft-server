6768a3868fcd0154c7645c826d5372a4c9acee83
Yogeshkumar-24 <YOGESHKUMAR_R@outlook.com>
2024-02-28 21:41:58 +0530

    cors updated

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const configureSocketIO = require('./socketIOConfig.js');
+const cors = require('cors')
 
 // import configureSocketIO from "./socketIOConfig"
 const app = express();
 const server = http.createServer(app);
-
+app.use(cors());
 const port = 8800;
 
 const io = new Server(server, {
   cors: {  
-    origin: '*',
+    origin: 'https://speedcraft.vercel.app',
+    methods: ['GET','POST']
   },
 });
 
