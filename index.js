f382977d3d3da620a344a8cdddf0ced0c88c33d3
Yogeshkumar-24 <YOGESHKUMAR_R@outlook.com>
2024-02-28 22:01:26 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
+// import express and http
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const configureSocketIO = require('./socketIOConfig.js');
+const cors = require('cors')
 
+// import configureSocketIO from "./socketIOConfig"
 const app = express();
 const server = http.createServer(app);
+app.use(cors());
+const port = process.env.port || 8800;
 
-const port = 8800;
-
-// CORS middleware to allow requests from all origins
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
+const io = new Server(server, {
+  cors: {  
+    origin: 'https://speedcraft.vercel.app/room',
+    methods: ['GET','POST']
+  },
 });
 
-const io = new Server(server);
 
-configureSocketIO(io);
+
+configureSocketIO(io); 
+    
 
 server.listen(port, () => {
   console.log('Server Started');
