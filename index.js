11c322b73c6e54f1674a60552ba281bd7514775e
Yogesh Kumar <97374934+Yogeshkumar-24@users.noreply.github.com>
2024-09-08 12:30:50 +0530

    Update cors url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.port || 8800;
 
 const io = new Server(server, {
   cors: {  
-    origin: 'http://localhost:3000/room',
+    origin: 'https://speedcraft.vercel.app/room',
     methods: ['GET','POST']
   },
 });
