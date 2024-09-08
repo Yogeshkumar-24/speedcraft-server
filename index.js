be019f116328b1423180356bc1c1bf0c8915da34
Yogesh Kumar <97374934+Yogeshkumar-24@users.noreply.github.com>
2024-09-08 12:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.port || 8800;
 
 const io = new Server(server, {
   cors: {  
-    origin: 'https://speedcraft.vercel.app/room',
+    origin: 'https://speedcraft.vercel.app/',
     methods: ['GET','POST']
   },
 });
